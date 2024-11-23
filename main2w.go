87dc44d528a251d5ae4b632567c0fc38c236a365
vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>
2024-11-23 21:46:53 +0530

    test GET, POST, DELETE  endpoints
    Signed-off-by: vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>

--- main2w.go
+++ main2w.go
@@ -1,48 +1,48 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"time"
+// import (
+// 	"fmt"
+// 	"log"
+// 	"time"
 
-	"github.com/golang-collections/collections/queue"
-	"github.com/google/uuid"
-	"github.com/vishalvivekm/cube/task"
-	"github.com/vishalvivekm/cube/worker"
-)
+// 	"github.com/golang-collections/collections/queue"
+// 	"github.com/google/uuid"
+// 	"github.com/vishalvivekm/cube/task"
+// 	"github.com/vishalvivekm/cube/worker"
+// )
 
-func main() {
-	db := make(map[uuid.UUID]*task.Task)
-	w := worker.Worker{
-		Queue: *queue.New(),
-		Db: db,
-	}
+// func main() {
+// 	db := make(map[uuid.UUID]*task.Task)
+// 	w := worker.Worker{
+// 		Queue: *queue.New(),
+// 		Db: db,
+// 	}
 
-	t := task.Task{
-		ID: uuid.New(),
-		Name: "test-container-1",
-		State: task.Scheduled,
-		// Image: "strm/helloworld-http",
-		Image: "vishalvivekm/to-do-app",
-	}
+// 	t := task.Task{
+// 		ID: uuid.New(),
+// 		Name: "test-container-1",
+// 		State: task.Scheduled,
+// 		// Image: "strm/helloworld-http",
+// 		Image: "vishalvivekm/to-do-app",
+// 	}
 
-	fmt.Println("starting task")
-	w.AddTask(t)
-	res := w.RunTask()
-	if res.Error != nil {
-		log.Fatalln(res.Error)
-	}
+// 	fmt.Println("starting task")
+// 	w.AddTask(t)
+// 	res := w.RunTask()
+// 	if res.Error != nil {
+// 		log.Fatalln(res.Error)
+// 	}
 
-	t.ContainerID = res.ContainerId
-	fmt.Printf("task %s in container %s\n", t.ID, t.ContainerID)
-	fmt.Println("sleeping...")
-	time.Sleep(time.Second * 60)
+// 	t.ContainerID = res.ContainerId
+// 	fmt.Printf("task %s in container %s\n", t.ID, t.ContainerID)
+// 	fmt.Println("sleeping...")
+// 	time.Sleep(time.Second * 60)
 
-	fmt.Printf("stopping task %s\n", t.ID)
-	t.State = task.Completed
-	w.AddTask(t)
-	res = w.RunTask()
-	if res.Error != nil {
-		log.Fatalln(res.Error)
-	}
-}
+// 	fmt.Printf("stopping task %s\n", t.ID)
+// 	t.State = task.Completed
+// 	w.AddTask(t)
+// 	res = w.RunTask()
+// 	if res.Error != nil {
+// 		log.Fatalln(res.Error)
+// 	}
+// }
---
+++ main3api.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+	"github.com/vishalvivekm/cube/task"
+	"github.com/vishalvivekm/cube/worker"
+)
+
+func main() {
+	host := os.Getenv("CUBE_HOST")
+	port, _ := strconv.Atoi(os.Getenv("CUBE_PORT"))
+	fmt.Printf("starting cube worker at %s:%d\n", host, port)
+
+	w := worker.Worker {
+		Queue: *queue.New(),
+		Db: make(map[uuid.UUID]*task.Task),
+	}
+	api := worker.Api{
+		Address: host,
+		Port: port,
+		Worker: &w,
+	}
+	go runTasks(&w)
+	api.Start()
+}
+
+func runTasks(w *worker.Worker) {
+	for {
+		if w.Queue.Len() != 0 {
+			res := w.RunTask()
+			if res.Error != nil {
+				log.Printf("error running task: %v\n", res.Error)
+			}
+		} else {
+				log.Printf("no tasks to process currently. \n")
+		}
+		log.Println("sleeping for 15 seconds")
+		time.Sleep(15 * time.Second)
+
+	}
+}
