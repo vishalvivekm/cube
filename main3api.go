a82cf4ecdafe1b40310f942e2193633b30aad7fe
vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>
2026-06-07 19:15:39 +0530

    add driver code for manager
    Signed-off-by: vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>

--- main3api.go
+++ main3api.go
@@ -1,49 +1,49 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"time"
+// import (
+// 	"fmt"
+// 	"log"
+// 	"os"
+// 	"strconv"
+// 	"time"
 
-	"github.com/golang-collections/collections/queue"
-	"github.com/google/uuid"
-	"github.com/vishalvivekm/cube/task"
-	"github.com/vishalvivekm/cube/worker"
-)
+// 	"github.com/golang-collections/collections/queue"
+// 	"github.com/google/uuid"
+// 	"github.com/vishalvivekm/cube/task"
+// 	"github.com/vishalvivekm/cube/worker"
+// )
 
-func main() {
-	host := os.Getenv("CUBE_HOST")
-	port, _ := strconv.Atoi(os.Getenv("CUBE_PORT"))
-	fmt.Printf("starting cube worker at %s:%d\n", host, port)
+// func main() {
+// 	host := os.Getenv("CUBE_HOST")
+// 	port, _ := strconv.Atoi(os.Getenv("CUBE_PORT"))
+// 	fmt.Printf("starting cube worker at %s:%d\n", host, port)
 
-	w := worker.Worker {
-		Queue: *queue.New(),
-		Db: make(map[uuid.UUID]*task.Task),
-	}
-	api := worker.Api{
-		Address: host,
-		Port: port,
-		Worker: &w,
-	}
-	go runTasks(&w)
-	go w.CollectStats()
-	api.Start()
-}
+// 	w := worker.Worker {
+// 		Queue: *queue.New(),
+// 		Db: make(map[uuid.UUID]*task.Task),
+// 	}
+// 	api := worker.Api{
+// 		Address: host,
+// 		Port: port,
+// 		Worker: &w,
+// 	}
+// 	go runTasks(&w)
+// 	go w.CollectStats()
+// 	api.Start()
+// }
 
-func runTasks(w *worker.Worker) {
-	for {
-		if w.Queue.Len() != 0 {
-			res := w.RunTask()
-			if res.Error != nil {
-				log.Printf("error running task: %v\n", res.Error)
-			}
-		} else {
-				log.Printf("no tasks to process currently. \n")
-		}
-		log.Println("sleeping for 15 seconds")
-		time.Sleep(15 * time.Second)
+// func runTasks(w *worker.Worker) {
+// 	for {
+// 		if w.Queue.Len() != 0 {
+// 			res := w.RunTask()
+// 			if res.Error != nil {
+// 				log.Printf("error running task: %v\n", res.Error)
+// 			}
+// 		} else {
+// 				log.Printf("no tasks to process currently. \n")
+// 		}
+// 		log.Println("sleeping for 15 seconds")
+// 		time.Sleep(15 * time.Second)
 
-	}
-}
+// 	}
+// }
---
+++ main4manager.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+	"github.com/vishalvivekm/cube/task"
+	"github.com/vishalvivekm/cube/worker"
+	"github.com/vishalvivekm/cube/manager"
+)
+
+func main() {
+	host := os.Getenv("CUBE_HOST")
+	port, _ := strconv.Atoi(os.Getenv("CUBE_PORT"))
+	fmt.Printf("starting cube worker at %s:%d\n", host, port)
+
+	w := worker.Worker {
+		Queue: *queue.New(),
+		Db: make(map[uuid.UUID]*task.Task),
+	}
+	api := worker.Api{
+		Address: host,
+		Port: port,
+		Worker: &w,
+	}
+	go runTasks(&w)
+	go w.CollectStats()
+	go api.Start()
+	workers := []string{fmt.Sprintf("%s:%d", host, port)}
+	m := manager.New(workers)
+	for i := 0; i < 3; i++ {
+		t := task.Task {
+			ID: uuid.New(), 
+			Name: fmt.Sprintf("test-container-%d", i),
+			State: task.Scheduled, 
+			Image: "vishalvivekm/to-do-app:latest", 
+		}
+		te := task.TaskEvent {
+			ID: uuid.New(),
+			State: task.Running, 
+			Task: t,
+		}
+		m.AddTask(te)
+		m.SendWork()
+
+		go func(){
+			for {
+				fmt.Printf("[Manager] Updating tasks from %d workers\n", len(m.Workers))
+				m.UpdateTasks()
+				time.Sleep(15 * time.Second)
+			}
+		}()
+		for _, t := range m.TaskDb{
+			fmt.Printf("[Manager] Task: id: %s, state: %d\n", t.ID, t.State)
+			time.Sleep((15 * time.Second))
+		}
+	}
+}
+
+func runTasks(w *worker.Worker) {
+	for {
+		if w.Queue.Len() != 0 {
+			res := w.RunTask()
+			if res.Error != nil {
+				log.Printf("error running task: %v\n", res.Error)
+			}
+		} else {
+				log.Printf("no tasks to process currently. \n")
+		}
+		log.Println("sleeping for 15 seconds")
+		time.Sleep(15 * time.Second)
+
+	}
+}
